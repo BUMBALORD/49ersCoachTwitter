3d507e7d537e01a99c6b231e28b6a5e696802f6a
DAVID HALLINAN <davidhallinan@DAVIDs-MacBook-Pro-2.local>
2016-01-11 14:45:11 -0800

    change logo and add script tags for fonts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    # @tweets = $client.user_timeline[0..99]
     @niners = $client.search("49ers coach", result_type: "recent").take(100).collect
   end
 end
